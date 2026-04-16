1b53e9f75a882c26c72e53e24dc462ecaf7f42ff
Jeison <jsolorzano26@aoiths.org>
2026-04-16 13:44:22 -0400

    Complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,27 @@
-mapboxgl.accessToken = "";
+
+const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoianNvbG9yemFubzI2IiwiYSI6ImNtbzFyZzJlbTBtYTEyb3B1dzJ0aWI4cHMifQ.hDQrk0kII7YUCE3jE0NOLQ";
+if (typeof MAPBOX_ACCESS_TOKEN === 'undefined') {
+    console.error("Mapbox token is missing. Check your env.js file.");
+}
+
+mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
+
 
 const map = new mapboxgl.Map({
-	container: 'map',
-	center: [-74.5, 40],
-	zoom: 9
-});	
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [-74.0060, 40.7128],
+    zoom: 10
+});
+
+
+map.addControl(new mapboxgl.NavigationControl());
+
+
+const popup = new mapboxgl.Popup({ offset: 25 })
+    .setText("Hello from NYC!");
+
+new mapboxgl.Marker({ color: "red" })
+    .setLngLat([-74.0060, 40.7128])
+    .setPopup(popup)
+    .addTo(map);
